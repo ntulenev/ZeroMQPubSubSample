f3fc3c835135360fbba12d6972c782e72df50072
Nikita Tulenev <n1tulenev@gmail.com>
2021-05-29 21:04:37 +0300

    Add serialization layer

---
+++ src/Common/Serialization/Message.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace ZeroMQPubSubSample.Common.Serialization
+{
+    /// <summary>
+    /// Transport message model.
+    /// </summary>
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Message
+    {
+        /// <summary>
+        /// Message key.
+        /// </summary>
+        [JsonProperty("key")]
+        [JsonRequired]
+        public long Key { get; set; }
+
+        /// <summary>
+        /// Message value.
+        /// </summary>
+        [JsonProperty("value")]
+        [JsonRequired]
+        public string Value { get; set; } = default!;
+    }
+}
---
+++ src/Common/Serialization/MessageConvertor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using Domain = ZeroMQPubSubSample.Common.Models;
+
+namespace ZeroMQPubSubSample.Common.Serialization
+{
+    /// <summary>
+    /// Helper that converts Buisness and Transport models.
+    /// </summary>
+    public static class MessageConvertor
+    {
+        /// <summary>
+        /// Create <see cref="Domain.Message"/> from <see cref="Message"/>.
+        /// </summary>
+        /// <param name="msg">Buisness message model.</param>
+        /// <returns>Transport message model.</returns>
+        public static Domain.Message FromTransport(this Message msg)
+        {
+            if (msg is null)
+            {
+                throw new ArgumentNullException(nameof(msg));
+            }
+
+            if (msg.Value is null)
+            {
+                throw new ArgumentException("Message Value is not set.", nameof(msg));
+            }
+
+            if (string.IsNullOrWhiteSpace(msg.Value))
+            {
+                throw new ArgumentException("Message Value is empty of contains only whitespaces.", nameof(msg));
+            }
+
+            return new Domain.Message(msg.Key, msg.Value);
+        }
+
+        /// <summary>
+        /// Create <see cref="Message"/> from <see cref="Domain.Message"/>.
+        /// </summary>
+        /// <param name="msg">Transport message model.</param>
+        /// <returns>Buisness message model.</returns>
+        public static Message ToTransport(this Domain.Message msg)
+        {
+            if (msg is null)
+            {
+                throw new ArgumentNullException(nameof(msg));
+            }
+
+            return new Message
+            {
+                Key = msg.Key,
+                Value = msg.Value
+            };
+        }
+    }
+}
