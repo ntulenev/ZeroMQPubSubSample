e77b8bd868ff9fbbfd7c9f173176b118c35d4ba0
Nikita Tulenev <n1tulenev@gmail.com>
2021-05-30 09:44:34 +0300

    Add MessageSenderConfiguration

---
+++ src/Generator/Logic/Configuration/MessageSenderConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace ZeroMQPubSubSample.Generator.Logic.Configuration
+{
+    public class MessageSenderConfiguration
+    {
+        public int SendHighWatermark { get; set; }
+
+        public string Address { get; set; }
+    }
+}
