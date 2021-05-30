63d703eb5ebc4287eb3bbe4b34c6980feeffb99c
Nikita Tulenev <n1tulenev@gmail.com>
2021-05-30 09:57:28 +0300

    Add IMemoryChannelProcessor

---
+++ src/Generator/Abstractions/IMemoryChannelProcessor.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ZeroMQPubSubSample.Generator.Abstractions
+{
+    public interface IMemoryChannelProcessor
+    {
+        public Task ProcessAsync(CancellationToken ct);
+
+    }
+}
