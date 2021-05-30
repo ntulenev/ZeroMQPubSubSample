8b88eff7a1221b9a268e3b9d499a5deba7b84a96
Nikita Tulenev <n1tulenev@gmail.com>
2021-05-30 08:49:43 +0300

    Add IMessageSender

---
+++ src/Generator/Abstractions/IMessageSender.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using ZeroMQPubSubSample.Common.Models;
+
+namespace ZeroMQPubSubSample.Generator.Abstractions
+{
+    public interface IMessageSender
+    {
+        public Task SendMessageAsync(Message message, CancellationToken ct);
+    }
+}
