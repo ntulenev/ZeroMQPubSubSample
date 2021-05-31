9dae85c991a292f89e1c5eda08de5fa6716a1234
Nikita Tulenev <n1tulenev@gmail.com>
2021-05-31 18:48:01 +0300

    Add SenderService

---
+++ src/Generator/Service/Services/SenderService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using ZeroMQPubSubSample.Generator.Abstractions;
+
+namespace ZeroMQPubSubSample.Generator.Service.Services
+{
+    /// <summary>
+    /// Hosted service that sends test data messages
+    /// </summary>
+    public class SenderService : IHostedService
+    {
+        public SenderService(ILogger<SenderService> logger,
+                             IHostApplicationLifetime hostApplicationLifetime,
+                             IMemoryChannelProcessor processor
+                                    )
+        {
+            _logger = logger;
+            _hostApplicationLifetime = hostApplicationLifetime ?? throw new ArgumentNullException(nameof(hostApplicationLifetime));
+            _processor = processor ?? throw new ArgumentNullException(nameof(processor));
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                _stopper = Task.Run(async () => await _processor
+                                            .ProcessAsync(_hostApplicationLifetime.ApplicationStopping).ConfigureAwait(false));
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogCritical(ex, "Error starting SenderService.");
+                _hostApplicationLifetime.StopApplication();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken) => await _stopper.ConfigureAwait(false);
+
+        private readonly ILogger<SenderService> _logger;
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly IMemoryChannelProcessor _processor;
+        private Task _stopper = default!;
+    }
+}
