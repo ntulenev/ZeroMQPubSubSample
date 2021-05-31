9044b870849311fd15960560ec1a44f1416d1e79
Nikita Tulenev <n1tulenev@gmail.com>
2021-05-31 18:29:49 +0300

    Add Service startup

--- src/Generator/Service/Startup.cs
+++ src/Generator/Service/Startup.cs
@@ -1,7 +1,17 @@
+using System;
+using System.Collections.Generic;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using ZeroMQPubSubSample.Generator.Abstractions;
+using ZeroMQPubSubSample.Generator.Logic;
+using ZeroMQPubSubSample.Generator.Logic.Configuration;
+using ZeroMQPubSubSample.Generator.Service.Services;
 
 namespace ZeroMQPubSubSample.Generator.Service
 {
@@ -17,6 +27,26 @@ namespace ZeroMQPubSubSample.Generator.Service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHealthChecks();
+
+            services.AddSingleton(RegisterGenerators);
+            services.AddSingleton<IMessageMemoryChannel, MessageMemoryChannel>();
+            services.Configure<MessageMemoryChannelConfiguration>(Configuration.GetSection(nameof(MessageMemoryChannelConfiguration)));
+
+            services.AddHostedService<GenerationService>();
+        }
+
+        private IEnumerable<IDataGenerator> RegisterGenerators(IServiceProvider serviceProvider)
+        {
+            List<IDataGenerator> generators = new();
+            IConfigurationSection generatorConfigs = Configuration.GetSection("Generators");
+            foreach (IConfigurationSection generatorConfig in generatorConfigs.GetChildren())
+            {
+                generators.Add(new DataGenerator(serviceProvider.GetRequiredService<ILogger<DataGenerator>>(),
+                                                 Options.Create(generatorConfig.Get<DataGeneratorConfiguration>()),
+                                                 serviceProvider.GetRequiredService<IMessageMemoryChannel>()));
+            }
+
+            return generators;
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
