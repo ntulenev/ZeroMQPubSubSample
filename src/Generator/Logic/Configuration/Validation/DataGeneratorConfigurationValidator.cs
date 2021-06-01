b8fd847a704fbe453ca89ce48ffea4b0ec9938e1
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-01 17:28:11 +0300

    Add DataGeneratorConfigurationValidator rules

--- src/Generator/Logic/Configuration/Validation/DataGeneratorConfigurationValidator.cs
+++ src/Generator/Logic/Configuration/Validation/DataGeneratorConfigurationValidator.cs
@@ -8,7 +8,27 @@ namespace ZeroMQPubSubSample.Generator.Logic.Configuration.Validation
     {
         public ValidateOptionsResult Validate(string name, DataGeneratorConfiguration options)
         {
-            throw new NotImplementedException();
+            if (options is null)
+            {
+                return ValidateOptionsResult.Fail("Configuration object is null.");
+            }
+
+            if (options.Destination is null)
+            {
+                return ValidateOptionsResult.Fail("Destination is null.");
+            }
+
+            if (string.IsNullOrWhiteSpace(options.Destination))
+            {
+                return ValidateOptionsResult.Fail("Destination not set.");
+            }
+
+            if (options.GenerationPeriodSeconds <= TimeSpan.Zero)
+            {
+                return ValidateOptionsResult.Fail("GenerationPeriodSeconds should be more than zero.");
+            }
+
+            return ValidateOptionsResult.Success;
         }
     }
 }
