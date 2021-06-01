4e83b3d62502fd92f9cbd321f0d87e3badcf9b74
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-01 17:30:01 +0300

    Add MessageSenderConfigurationValidator rules

--- src/Generator/Logic/Configuration/Validation/MessageSenderConfigurationValidator.cs
+++ src/Generator/Logic/Configuration/Validation/MessageSenderConfigurationValidator.cs
@@ -8,7 +8,27 @@ namespace ZeroMQPubSubSample.Generator.Logic.Configuration.Validation
     {
         public ValidateOptionsResult Validate(string name, MessageSenderConfiguration options)
         {
-            throw new NotImplementedException();
+            if (options is null)
+            {
+                return ValidateOptionsResult.Fail("Configuration object is null.");
+            }
+
+            if (options.SendHighWatermark <= 0)
+            {
+                return ValidateOptionsResult.Fail("SendHighWatermark should be more than zero.");
+            }
+
+            if (options.Address is null)
+            {
+                return ValidateOptionsResult.Fail("Address is null.");
+            }
+
+            if (string.IsNullOrWhiteSpace(options.Address))
+            {
+                return ValidateOptionsResult.Fail("Address not set.");
+            }
+
+            return ValidateOptionsResult.Success;
         }
     }
 }
