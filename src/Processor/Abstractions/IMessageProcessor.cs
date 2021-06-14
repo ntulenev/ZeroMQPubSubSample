a0f4a9adfc377a875b122d1417d42c8ebcc2d395
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-14 22:34:56 +0300

    Add IMessageProcessor

---
+++ src/Processor/Abstractions/IMessageProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using ZeroMQPubSubSample.Common.Models;
+
+namespace ZeroMQPubSubSample.Processor.Abstractions
+{
+    public interface IMessageProcessor
+    {
+        public Task ProcessAsync(Message message, CancellationToken ct);
+    }
+}
