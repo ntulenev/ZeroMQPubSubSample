d894458e9d3ac9b6d95fb4e56c286d6a8b4d9766
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-14 22:49:51 +0300

    Add MessageReceiverConfiguration

---
+++ src/Processor/Logic/Configuration/MessageReceiverConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace ZeroMQPubSubSample.Processor.Logic.Configuration
+{
+    class MessageReceiverConfiguration
+    {
+        /// <summary>
+        /// NetMQ tcp socket address.
+        /// </summary>
+        public string Address { get; set; } = default!;
+    }
+}
