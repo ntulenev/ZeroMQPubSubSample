dd9607d55683b3fd77b5c5dda35d83dbde2f1022
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-14 22:34:49 +0300

    Add IMessageReceiver

---
+++ src/Processor/Abstractions/IMessageReceiver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+using ZeroMQPubSubSample.Common.Models;
+
+namespace ZeroMQPubSubSample.Processor.Abstractions
+{
+    public interface IMessageReceiver
+    {
+        public IAsyncEnumerable<Message> ReceiveAsync(CancellationToken ct);
+    }
+}
