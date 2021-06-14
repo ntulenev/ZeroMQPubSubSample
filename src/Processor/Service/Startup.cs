d2fbf57fc3da635652892ab437f9094c201fc7f4
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-14 22:49:34 +0300

    Add startap default setup

--- src/Processor/Service/Startup.cs
+++ src/Processor/Service/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using ZeroMQPubSubSample.Processor.Service.Services;
 
 namespace ZeroMQPubSubSample.Processor.Service
 {
@@ -25,12 +19,20 @@ namespace ZeroMQPubSubSample.Processor.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks();
+
+            //TODO Add IMessageProcessor 
+            //TODO Add IMessageReceiver
+
+            services.AddHostedService<ReceiverService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseRouting();
 
+            app.UseHealthChecks("/hc");
         }
     }
 }
