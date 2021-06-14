da4e9fe09d3719eb77fc60391df87a376d984a84
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-14 22:50:00 +0300

    Add MessageReceiverConfigurationValidator

---
+++ src/Processor/Logic/Configuration/Validation/MessageReceiverConfigurationValidator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace ZeroMQPubSubSample.Processor.Logic.Configuration.Validation
+{
+    class MessageReceiverConfigurationValidator : IValidateOptions<MessageReceiverConfiguration>
+    {
+        public ValidateOptionsResult Validate(string name, MessageReceiverConfiguration options)
+        {
+            if (options is null)
+            {
+                return ValidateOptionsResult.Fail("Configuration object is null.");
+            }
+
+            if (options.Address is null)
+            {
+                return ValidateOptionsResult.Fail("Address is null.");
+            }
+
+            if (string.IsNullOrWhiteSpace(options.Address))
+            {
+                return ValidateOptionsResult.Fail("Address not set.");
+            }
+
+            return ValidateOptionsResult.Success;
+        }
+    }
+}
