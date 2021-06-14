200549877c9d55834d2db554cdd956729bead1e8
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-14 22:44:03 +0300

    Add documentation

--- src/Processor/Abstractions/IMessageProcessor.cs
+++ src/Processor/Abstractions/IMessageProcessor.cs
@@ -5,8 +5,14 @@ using ZeroMQPubSubSample.Common.Models;
 
 namespace ZeroMQPubSubSample.Processor.Abstractions
 {
+    /// <summary>
+    /// Process messages from <see cref="IMessageReceiver"/>.
+    /// </summary>
     public interface IMessageProcessor
     {
+        /// <summary>
+        /// Process message
+        /// </summary>
         public Task ProcessAsync(Message message, CancellationToken ct);
     }
 }
--- src/Processor/Abstractions/IMessageReceiver.cs
+++ src/Processor/Abstractions/IMessageReceiver.cs
@@ -5,8 +5,15 @@ using ZeroMQPubSubSample.Common.Models;
 
 namespace ZeroMQPubSubSample.Processor.Abstractions
 {
+    /// <summary>
+    /// Receives messages from zeroMQ.
+    /// </summary>
     public interface IMessageReceiver
     {
+        /// <summary>
+        /// Returns messages from zeroMQ as <see cref="IAsyncEnumerable{T}"/>.
+        /// </summary>
+        /// <param name="ct">Cancellation token.</param>
         public IAsyncEnumerable<Message> ReceiveAsync(CancellationToken ct);
     }
 }
