44aa5c8edf7f5f2f4e97e9dbebb44a1ef55d54e2
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-15 21:58:12 +0300

    Extend MessageReceiverConfiguration

--- src/Processor/Logic/Configuration/MessageReceiverConfiguration.cs
+++ src/Processor/Logic/Configuration/MessageReceiverConfiguration.cs
@@ -6,5 +6,15 @@
         /// NetMQ tcp socket address.
         /// </summary>
         public string Address { get; set; } = default!;
+
+        /// <summary>
+        /// Limit on the maximum number of incomming messages ØMQ shall queue in memory.
+        /// </summary>
+        public int ReceiveHighWatermark { get; set; }
+
+        /// <summary>
+        /// Target queue
+        /// </summary>
+        public string Topic { get; set; } = default!;
     }
 }
