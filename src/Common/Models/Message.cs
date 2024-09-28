1da6d9e89b22e502a8acdf67378a4116cfc3de3f
Nikita Tulenev <n1tulenev@gmail.com>
2024-09-28 12:44:42 +0200

    Update models xml-doc

--- src/Common/Models/Message.cs
+++ src/Common/Models/Message.cs
@@ -1,6 +1,8 @@
 ﻿namespace ZeroMQPubSubSample.Common.Models;
 
 /// <summary>
-/// Represents example message.
+/// Represents a message with a key and a value.
 /// </summary>
+/// <param name="Key">The unique key associated with the message.</param>
+/// <param name="Value">The content or value of the message.</param>
 public record Message(long Key, string Value);
--- src/Common/Models/TargetedMessage.cs
+++ src/Common/Models/TargetedMessage.cs
@@ -1,6 +1,9 @@
 ﻿namespace ZeroMQPubSubSample.Common.Models;
 
 /// <summary>
-/// Represents message with targeted topic.
+/// Represents a targeted message that includes a key, value, and destination.
 /// </summary>
+/// <param name="Key">The unique key associated with the message.</param>
+/// <param name="Value">The content or value of the message.</param>
+/// <param name="Destination">The target destination for the message.</param>
 public sealed record TargetedMessage(long Key, string Value, string Destination) : Message(Key, Value);
