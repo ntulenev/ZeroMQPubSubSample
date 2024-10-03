a93ed143366564104da00cd827bbd5223ef59f62
Nikita Tulenev <n1tulenev@gmail.com>
2024-10-03 17:51:28 +0200

    Add IReceiveHandler

---
+++ src/Processor/Abstractions/IReceiveHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace ZeroMQPubSubSample.Processor.Abstractions;
+
+/// <summary>
+/// Defines a handler for receiving and processing tasks.
+/// </summary>
+public interface IReceiveHandler
+{
+    /// <summary>
+    /// Handles a task asynchronously.
+    /// </summary>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public Task HandleAsync(CancellationToken cancellationToken);
+}
