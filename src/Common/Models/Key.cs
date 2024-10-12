ba32630442f82cfddc41490e18a8a06a3389a9ea
Nikita Tulenev <n1tulenev@gmail.com>
2024-10-12 08:37:56 +0200

    Add strongly typed Key

---
+++ src/Common/Models/Key.cs
@@ -0,0 +1,7 @@
+﻿namespace ZeroMQPubSubSample.Common.Models;
+
+/// <summary>
+/// Represents a key with an identifier.
+/// </summary>
+/// <param name="Id">The identifier for the key.</param>
+public record struct Key(long Id);
