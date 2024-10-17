100d354f4ca6b386014b7a7c898cf94c119cde2a
Nikita Tulenev <n1tulenev@gmail.com>
2024-10-17 19:56:31 +0200

    Add IDestinationSender

---
+++ src/Generator/Abstractions/IDestinationSender.cs
@@ -0,0 +1,8 @@
+﻿using ZeroMQPubSubSample.Common.Models;
+
+namespace ZeroMQPubSubSample.Generator.Abstractions;
+
+public interface IDestinationSender : IDisposable
+{
+    public void Send(Destination destination, string message);
+}
