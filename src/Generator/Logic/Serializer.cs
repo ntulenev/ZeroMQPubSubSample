92143023d4eee93e058ac05bd7cb3ee2c3ff1150
Nikita Tulenev <n1tulenev@gmail.com>
2024-10-18 08:00:20 +0200

    Add Serializer

---
+++ src/Generator/Logic/Serializer.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using ZeroMQPubSubSample.Generator.Abstractions;
+using ZeroMQPubSubSample.Common.Serialization;
+
+
+using Domain = ZeroMQPubSubSample.Common.Models;
+
+namespace ZeroMQPubSubSample.Generator.Logic;
+
+/// <summary>
+/// A concrete implementation of the <see cref="ISerializer{From, To}"/> 
+/// interface that serializes a <see cref="Domain.Message"/> object into a JSON string.
+/// </summary>
+public sealed class Serializer : ISerializer<Domain.Message, string>
+{
+    /// <summary>
+    /// Serializes the provided <see cref="Domain.Message"/> into its JSON string representation.
+    /// </summary>
+    /// <param name="payload">The <see cref="Domain.Message"/> to serialize. Cannot be null.</param>
+    /// <returns>A JSON string representing the serialized <see cref="Domain.Message"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="payload"/> is null.</exception>
+    public string Serialize(Domain.Message payload)
+    {
+        ArgumentNullException.ThrowIfNull(payload);
+
+        var transport = payload.ToTransport();
+        return JsonConvert.SerializeObject(transport);
+    }
+}
+
