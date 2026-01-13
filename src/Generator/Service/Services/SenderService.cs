b751444e4b3ede1d0d467b192ef3254cd5195c0d
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-13 08:02:13 +0100

    Refactors SenderService for reliability
    Updates the SenderService to improve its reliability and error handling.
    
    Specifically:
    - Changes the service to internal, limiting its visibility.
    - Adds exception handling to prevent unhandled exceptions from crashing the application.
    - Disables specific CA warnings to address code analysis issues related to exception handling and task creation.

--- src/Generator/Service/Services/SenderService.cs
+++ src/Generator/Service/Services/SenderService.cs
@@ -1,11 +1,11 @@
-﻿using ZeroMQPubSubSample.Generator.Abstractions;
+using ZeroMQPubSubSample.Generator.Abstractions;
 
 namespace ZeroMQPubSubSample.Generator.Service.Services;
 
 /// <summary>
 /// Hosted service that sends test data messages.
 /// </summary>
-public sealed class SenderService : IHostedService
+internal sealed class SenderService : IHostedService
 {
     public SenderService(ILogger<SenderService> logger,
                          IHostApplicationLifetime hostApplicationLifetime,
@@ -25,25 +25,37 @@ public sealed class SenderService : IHostedService
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
+#pragma warning disable CA1031 // Do not catch general exception types
         try
         {
+#pragma warning disable CA2016 // Forward the 'CancellationToken' parameter to methods
             _stopper = Task.Run(async () => await _processor
                                         .ProcessAsync(_hostApplicationLifetime.ApplicationStopping).ConfigureAwait(false)
                                 );
-            _stopper.ContinueWith(x => _hostApplicationLifetime.StopApplication(), TaskContinuationOptions.OnlyOnFaulted);
+#pragma warning restore CA2016 // Forward the 'CancellationToken' parameter to methods
+#pragma warning disable CA2008 // Do not create tasks without passing a TaskScheduler
+            _ = _stopper.ContinueWith(x => _hostApplicationLifetime.StopApplication(), TaskContinuationOptions.OnlyOnFaulted);
+#pragma warning restore CA2008 // Do not create tasks without passing a TaskScheduler
         }
         catch (Exception ex)
         {
             _logger?.LogCritical(ex, "Error starting SenderService.");
             _hostApplicationLifetime.StopApplication();
         }
+#pragma warning restore CA1031 // Do not catch general exception types
 
         return Task.CompletedTask;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task StopAsync(CancellationToken cancellationToken)
     {
         _logger.LogInformation("Stopping service.");
+#pragma warning disable CA1031 // Do not catch general exception types
         try
         {
             await _stopper.ConfigureAwait(false);
@@ -56,6 +68,7 @@ public sealed class SenderService : IHostedService
         {
             _logger.LogError(ex, "Error on stopping service.");
         }
+#pragma warning restore CA1031 // Do not catch general exception types
     }
 
     private readonly ILogger _logger;
