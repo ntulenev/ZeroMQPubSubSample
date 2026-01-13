75a22d3936b2417005587250e8de225807360fd1
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-13 08:03:16 +0100

    Removes unnecessary public access modifier
    Removes the redundant public access modifier from the IMessageSender.SendMessageAsync method, as it is already implicitly public due to the interface declaration. This simplifies the code and improves readability without affecting functionality.

--- src/Generator/Abstractions/IMessageSender.cs
+++ src/Generator/Abstractions/IMessageSender.cs
@@ -1,4 +1,4 @@
-﻿using ZeroMQPubSubSample.Common.Models;
+using ZeroMQPubSubSample.Common.Models;
 
 namespace ZeroMQPubSubSample.Generator.Abstractions;
 
@@ -13,6 +13,6 @@ public interface IMessageSender
     /// <param name="message">The <see cref="TargetedMessage"/> to be sent.</param>
     /// <param name="ct">A <see cref="CancellationToken"/> to signal the cancellation of the operation if necessary.</param>
     /// <returns>A <see cref="Task"/> that represents the asynchronous send operation.</returns>
-    public Task SendMessageAsync(TargetedMessage message, CancellationToken ct);
+    Task SendMessageAsync(TargetedMessage message, CancellationToken ct);
 }
 
