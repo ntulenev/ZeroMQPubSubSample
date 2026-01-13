4e2c266cfc47f6cf1819129f0831860e04324f5c
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-13 08:03:11 +0100

    Defines a generic serializer interface
    Introduces an interface for serializing objects from one type to another.
    
    This abstraction enables the implementation of different serialization strategies
    without tightly coupling components to specific serialization libraries.

--- src/Generator/Abstractions/ISerializer.cs
+++ src/Generator/Abstractions/ISerializer.cs
@@ -1,17 +1,17 @@
-﻿namespace ZeroMQPubSubSample.Generator.Abstractions;
+namespace ZeroMQPubSubSample.Generator.Abstractions;
 
 /// <summary>
-/// Defines a generic interface for serializing an object of type <typeparamref name="From"/> 
-/// into an object of type <typeparamref name="To"/>.
+/// Defines a generic interface for serializing an object of type <typeparamref name="TFrom"/> 
+/// into an object of type <typeparamref name="TTo"/>.
 /// </summary>
-/// <typeparam name="From">The type of the object to serialize.</typeparam>
-/// <typeparam name="To">The type of the object that is the result of serialization.</typeparam>
-public interface ISerializer<From, To>
+/// <typeparam name="TFrom">The type of the object to serialize.</typeparam>
+/// <typeparam name="TTo">The type of the object that is the result of serialization.</typeparam>
+public interface ISerializer<TFrom, TTo>
 {
     /// <summary>
-    /// Serializes the given payload of type <typeparamref name="From"/> into an object of type <typeparamref name="To"/>.
+    /// Serializes the given payload of type <typeparamref name="TFrom"/> into an object of type <typeparamref name="TTo"/>.
     /// </summary>
     /// <param name="payload">The object to serialize.</param>
-    /// <returns>An object of type <typeparamref name="To"/> that represents the serialized form of the input payload.</returns>
-    public To Serialize(From payload);
+    /// <returns>An object of type <typeparamref name="TTo"/> that represents the serialized form of the input payload.</returns>
+    TTo Serialize(TFrom payload);
 }
