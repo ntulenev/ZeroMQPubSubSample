e385760881f86092b92bcc98a0832439884e47f9
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-13 08:02:34 +0100

    Improves message sending and disposal
    Refactors message sending to improve cancellation and resource management.
    
    Updates the Dispose method to adhere to best practices.
    Adds XML documentation for better clarity and maintainability.

--- src/Generator/Logic/MessageSender.cs
+++ src/Generator/Logic/MessageSender.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 
 using ZeroMQPubSubSample.Generator.Abstractions;
 
@@ -14,8 +14,8 @@ public class MessageSender : IMessageSender, IDisposable
     /// <summary>
     /// Creates <see cref="MessageSender"/>.
     /// </summary>
+    /// <param name="sender">Sender.</param>
     /// <param name="logger">Logger.</param>
-    /// <param name="options">Config params.</param>
     public MessageSender(IDestinationSender<Domain.Message> sender,
                          ILogger<MessageSender> logger)
     {
@@ -35,23 +35,22 @@ public class MessageSender : IMessageSender, IDisposable
 
         ThrowIfDisposed();
 
-        using var _ = _logger.BeginScope("Sending message {message}", message);
+        using var _ = _logger.BeginScope("Sending message {Message}", message);
 
         var sendTask = Task.Run(() => SendMessage(message), ct);
 
         TaskCompletionSource cancelTaskSource = new(TaskCreationOptions.RunContinuationsAsynchronously);
-        ct.Register(cancelTaskSource.SetResult);
+        var __ = ct.Register(cancelTaskSource.SetResult);
 
         await Task.WhenAny(cancelTaskSource.Task, sendTask).Unwrap().ConfigureAwait(false);
     }
 
-    private void SendMessage(Domain.TargetedMessage message)
-    {
-        _sender.Send(message.Destination, message);
-    }
+    private void SendMessage(Domain.TargetedMessage message) => _sender.Send(message.Destination, message);
 
     /// <inheritdoc/>
+#pragma warning disable CA1063 // Implement IDisposable Correctly
     public void Dispose()
+#pragma warning restore CA1063 // Implement IDisposable Correctly
     {
         if (_isDisposed)
         {
@@ -66,11 +65,21 @@ public class MessageSender : IMessageSender, IDisposable
         GC.SuppressFinalize(this);
     }
 
-    protected void ThrowIfDisposed()
-    {
-        ObjectDisposedException.ThrowIf(_isDisposed,this);
-    }
+    /// <summary>
+    /// Throws an ObjectDisposedException if the current instance has been disposed.
+    /// </summary>
+    /// <remarks>Call this method at the beginning of operations that require the object to be in a valid,
+    /// undisposed state. This helps prevent access to resources that have already been released.</remarks>
+    protected void ThrowIfDisposed() => ObjectDisposedException.ThrowIf(_isDisposed, this);
 
+
+    /// <summary>
+    /// Releases the unmanaged resources used by the object and optionally releases the managed resources.
+    /// </summary>
+    /// <remarks>This method is called by public Dispose methods and can be overridden to release additional
+    /// resources. When disposing is true, both managed and unmanaged resources can be disposed; when false, only
+    /// unmanaged resources should be released.</remarks>
+    /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
     protected virtual void Dispose(bool disposing)
     {
         if (disposing)
