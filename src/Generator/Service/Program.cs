1efa5d11b7a853be289d0416e4091588a7e34146
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-13 08:02:29 +0100

    Recreates Program.cs
    Fixes an issue where the original Program.cs file was unintentionally removed,
    this commit re-adds and ensures the correct startup configuration for the
    Generator service.

--- src/Generator/Service/Program.cs
+++ src/Generator/Service/Program.cs
@@ -1,9 +1,6 @@
 using ZeroMQPubSubSample.Generator.Service;
 
 var builder = Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-var app = builder.Build(); 
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+var app = builder.Build();
 app.Run();
