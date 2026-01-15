d43db3db9efb2a52e2a46e7d2e44404cc46df587
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-15 07:41:11 +0100

    Makes Capacity a required property.
    Ensures that the Capacity property of the MessageMemoryChannelConfiguration is always initialized, preventing potential null reference exceptions.

--- src/Generator/Logic/Configuration/MessageMemoryChannelConfiguration.cs
+++ src/Generator/Logic/Configuration/MessageMemoryChannelConfiguration.cs
@@ -1,4 +1,4 @@
-﻿namespace ZeroMQPubSubSample.Generator.Logic.Configuration;
+namespace ZeroMQPubSubSample.Generator.Logic.Configuration;
 
 /// <summary>
 /// Configuration for <see cref="MessageMemoryChannel"/>.
@@ -8,5 +8,5 @@ public sealed class MessageMemoryChannelConfiguration
     /// <summary>
     /// In-memory channel max size.
     /// </summary>
-    public int Capacity { get; init; }
+    public required int Capacity { get; init; }
 }
